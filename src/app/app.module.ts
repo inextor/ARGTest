6fd8ecb8e05a73623c519d47b57e8fd85b235a5f
nextor <nextor@gmail.com>
2019-12-17 12:57:13 -0800

    Adding styles and removing default data from angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './pages/base/base.component';
+import { LoadingComponent } from './components/loading/loading.component';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    LoadingComponent
+    NavigationMenuComponent,
+    HeaderComponent,
+    PaginationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
